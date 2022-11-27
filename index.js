feb9bc7be54d50b92ab4bc1c2e3759c25abfe904
unknown <julianatonu@gmail.com>
2022-11-27 23:54:21 +0600

    verify advertise

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//jwt
+function verifyJWT(req, res, next){
+    // console.log(req.headers.authorization)
+    const authHeader =req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).send({message:'unauthorized access'})
+    }
+const token =authHeader.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+if(err){
+    return res.status(403).send({message: 'unauthorized access'})
+}
+req.decoded = decoded;
+next();
+    })
+}
+
+
 async function run(){
 try{
 const categoriesCollection=client.db('resaledb').collection('categories')
@@ -204,7 +222,7 @@ app.get ('/bookings', async(req, res)=>{
 })
 
 // advertise
-app.post('/advertise',async(req,res)=>{
+app.post('/advertise', verifyJWT,async(req,res)=>{
     const product=req.body;
     console.log(product)
     const result =await advertiseCollection.insertOne(product)
